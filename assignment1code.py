d772163ce350cb487175acbae73665991c7760a7
“RZhang370902” <zhangruike2413@gmail.com>
2024-05-09 09:49:09 -0600

    assignment 1 done

--- assignment1code.py
+++ assignment1code.py
@@ -2,9 +2,12 @@
 # Assignment 1 Starter Code
 # Add your name here!
 
-studentName = "Sarah Shah"  # Change the contents of this variable to your name (leave the quotation marks).
+studentName = "Rick Zhang"  # Change the contents of this variable to your name (leave the quotation marks).
 
 print("Hello, World!")
 print("My name is " + studentName + " and I am a student in ENSF 692.")
 
+best_skill = "Programming in Python"
+
 # print your best skill here
+print("My best skill is:", best_skill)
